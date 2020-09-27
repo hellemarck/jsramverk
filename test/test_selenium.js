4aba84a7b6355e1930c7b1a6b309aa58e915758e
Mette Hellemarck <mettehellemarck@hotmail.com>
2020-09-27 16:11:31 +0200

    debugging travis...

--- test/test_selenium.js
+++ test/test_selenium.js
@@ -2,23 +2,23 @@
  * Test for getting started with Selenium.
  * could also run i chrome, dsnt matter
  */
-
-"use strict";
-
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/firefox');
-const chromedriver = require('chromedriver');
-const firefox = require('selenium-webdriver/firefox')
-
-var browser = new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.firefox())
-    .build();
-
-browser.get("http://localhost:3000/reports/week/1");
-
-// Way 2
-browser.getTitle().then(function(title) {
-    console.log(title);
-});
-
-browser.quit();
+// 
+// "use strict";
+//
+// const webdriver = require('selenium-webdriver');
+// const chrome = require('selenium-webdriver/firefox');
+// const chromedriver = require('chromedriver');
+// const firefox = require('selenium-webdriver/firefox')
+//
+// var browser = new webdriver.Builder().
+//     withCapabilities(webdriver.Capabilities.firefox())
+//     .build();
+//
+// browser.get("http://localhost:3000/reports/week/1");
+//
+// // Way 2
+// browser.getTitle().then(function(title) {
+//     console.log(title);
+// });
+//
+// browser.quit();
